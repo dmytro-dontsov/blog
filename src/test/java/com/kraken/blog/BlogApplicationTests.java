8e9cbbfa1ac42863fed9fda4865922d6c5eaaf71
dmytro-dontsov <qemmnes@gmail.com>
2024-11-05 17:36:08 +0100

    Create project, adding dependencies

---
+++ src/test/java/com/kraken/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kraken.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
